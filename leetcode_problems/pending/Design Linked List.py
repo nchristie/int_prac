099b655dd1bd9ceeee5231934e1f41f11d511551
Naomi Christie <naomi.christie@gmail.com>
2018-07-28 12:28:35 +0000

    Add some work on Design Linked List

--- leetcode_problems/pending/Design Linked List.py
+++ leetcode_problems/pending/Design Linked List.py
@@ -1,13 +1,26 @@
 '''
-Design your implementation of the linked list. You can choose to use the singly linked list or the doubly linked list. A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node. If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
+Design your implementation of the linked list. You can choose to use the singly
+linked list or the doubly linked list. A node in a singly linked list should
+have two attributes: val and next. val is the value of the current node, and
+next is a pointer/reference to the next node. If you want to use the doubly
+linked list, you will need one more attribute prev to indicate the previous
+node in the linked list. Assume all nodes in the linked list are 0-indexed.
 
 Implement these functions in your linked list class:
 
-get(index) : Get the value of the index-th node in the linked list. If the index is invalid, return -1.
-addAtHead(val) : Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
-addAtTail(val) : Append a node of value val to the last element of the linked list.
-addAtIndex(index, val) : Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
-deleteAtIndex(index) : Delete the index-th node in the linked list, if the index is valid.
+get(index) : Get the value of the index-th node in the linked list. If the
+index is invalid, return -1.
+addAtHead(val) : Add a node of value val before the first element of the linked
+list. After the insertion, the new node will be the first node of the linked
+list.
+addAtTail(val) : Append a node of value val to the last element of the linked
+list.
+addAtIndex(index, val) : Add a node of value val before the index-th node in
+the linked list. If index equals to the length of linked list, the node will
+be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+deleteAtIndex(index) : Delete the index-th node in the linked list, if the
+index is valid.
+
 Example:
 
 MyLinkedList linkedList = new MyLinkedList();
@@ -26,25 +39,45 @@ Please do not use the built-in LinkedList library.
 '''
 
 
+class Node(object):
+    def __init__(self, val=None):
+        self.val = None
+        self.next_val = None
+
 class MyLinkedList(object):
+
     def __init__(self):
         """
         Initialize your data structure here.
         """
+        self.head = Node()
 
     def get(self, index):
         """
-        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        Get the value of the index-th node in the linked list. If the index is
+        invalid, return -1.
         :type index: int
         :rtype: int
         """
+        self.sought_index = self.head
+        for i in range(index):
+            self.sought_index = self.next
+        return self.sought_index
+            
+            
 
     def addAtHead(self, val):
         """
-        Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked
+        list.
         :type val: int
         :rtype: void
         """
+        new_node = Node()
+        new_node.val = val
+        new_node.next_val = self.head
+        self.head = new_node
 
     def addAtTail(self, val):
         """
@@ -52,6 +85,7 @@ class MyLinkedList(object):
         :type val: int
         :rtype: void
         """
+        pass
 
     def addAtIndex(self, index, val):
         """
@@ -60,6 +94,7 @@ class MyLinkedList(object):
         :type val: int
         :rtype: void
         """
+        pass
 
     def deleteAtIndex(self, index):
         """
@@ -67,13 +102,19 @@ class MyLinkedList(object):
         :type index: int
         :rtype: void
         """
+        pass
+
+#Your MyLinkedList object will be instantiated and called as such:
+obj = MyLinkedList()
+param_1 = obj.get(0)
+obj.addAtHead(4)
+#obj.addAtTail(val)
+#obj.addAtIndex(index,val)
+#obj.deleteAtIndex(index)
+
+print param_1.val
+param_2 = obj.get(0)
+print param_2.val
 
 
-
-        # Your MyLinkedList object will be instantiated and called as such:
-        # obj = MyLinkedList()
-        # param_1 = obj.get(index)
-        # obj.addAtHead(val)
-        # obj.addAtTail(val)
-        # obj.addAtIndex(index,val)
-        # obj.deleteAtIndex(index)
+       
