cbac113d2ef78449f3ba2e532d5e59b708628588
Naomi <nchristie5@bloomberg.net>
2018-08-15 17:36:27 +0100

    modify construct string from binary tree

--- leetcode_problems/pending/construct_string_from_binary_tree.py
+++ leetcode_problems/pending/construct_string_from_binary_tree.py
@@ -1,7 +1,10 @@
 '''
-You need to construct a string consists of parenthesis and integers from a binary tree with the preorder traversing way.
+You need to construct a string consists of parenthesis and integers from a binary
+tree with the preorder traversing way.
 
-The null node needs to be represented by empty parenthesis pair "()". And you need to omit all the empty parenthesis pairs that don't affect the one-to-one mapping relationship between the string and the original binary tree.
+The null node needs to be represented by empty parenthesis pair "()". And you need
+to omit all the empty parenthesis pairs that don't affect the one-to-one mapping
+relationship between the string and the original binary tree.
 
 Example 1:
 Input: Binary tree: [1,2,3,4]
@@ -27,7 +30,8 @@ Input: Binary tree: [1,2,3,null,4]
 Output: "1(2()(4))(3)"
 
 Explanation: Almost the same as the first example, 
-except we can't omit the first parenthesis pair to break the one-to-one mapping relationship between the input and the output.
+except we can't omit the first parenthesis pair to break the one-to-one mapping
+relationship between the input and the output.
 '''
 
 
